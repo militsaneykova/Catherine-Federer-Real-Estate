5573a4be19ecccb16b8890add7f297bfb6de478d
Militsa Neykova <militsaneykovany@gmail.com>
2017-09-14 21:28:01 -0400

    add create method in apartments controller

--- RealEstateApp/app/controllers/apartments_controller.rb
+++ RealEstateApp/app/controllers/apartments_controller.rb
@@ -1,2 +1,18 @@
 class ApartmentsController < ApplicationController
+    def index 
+    end
+
+    def new 
+        @apartment = Apartment.new
+    end
+
+    def create
+        @apartment = Apartment.new(apartment_params)
+    end
+
+    private
+
+    def apartment_params
+        params.require(:apartment).permit(:photo, :title, :address, :bedrooms, :bathrooms, :description, :price, :landlord_id)
+    end
 end
