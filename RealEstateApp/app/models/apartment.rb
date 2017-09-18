e97a222cc08752a2d4f1685e7a16e97889ac3984
Militsa Neykova <militsaneykovany@gmail.com>
2017-09-18 18:41:01 -0400

    add a relation for attachment file

--- RealEstateApp/app/models/apartment.rb
+++ RealEstateApp/app/models/apartment.rb
@@ -1,3 +1,5 @@
 class Apartment < ApplicationRecord
     belongs_to :user 
+    has_attached_file :apartment_img, styles: { apartment_index: "250x350>", apartment_show: "350x400>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :apartment_img, content_type: /\Aimage\/.*\z/
 end
