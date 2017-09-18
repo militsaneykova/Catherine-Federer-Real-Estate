ff28586acaffd02ef5e0cebc64ebab636597bf5f
Militsa Neykova <militsaneykovany@gmail.com>
2017-09-18 09:31:16 -0400

    add search field at index.html

--- RealEstateApp/config/routes.rb
+++ RealEstateApp/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # devise_for :installs
   root 'apartments#index'
   resources :apartments
+  get 'search_results' => 'apartments#search_results', as: 'search_results'
+
 end
