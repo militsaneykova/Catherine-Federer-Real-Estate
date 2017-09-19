69440d68d5a74afb34f681f6be03d6de41569c7e
Militsa Neykova <militsaneykovany@gmail.com>
2017-09-19 15:04:43 -0400

    add paperclip

--- RealEstateApp/db/migrate/20170918212347_add_attachment_apartment_img_to_apartments.rb
+++ RealEstateApp/db/migrate/20170918212347_add_attachment_apartment_img_to_apartments.rb
@@ -2,10 +2,13 @@ class AddAttachmentApartmentImgToApartments < ActiveRecord::Migration[5.1]
   def self.up
     change_table :apartments do |t|
       t.attachment :apartment_img
+      t.attachment :attachment_img1
+      t.attachment :attachment_img2
+      t.attachment :attachment_img3
     end
   end
 
   def self.down
-    remove_attachment :apartments, :apartment_img
+    remove_attachment :apartments, :apartment_img, :attachment_img1, :attachment_img2, :attachment_img3
   end
 end
